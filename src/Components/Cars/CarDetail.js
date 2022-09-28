c23486ddacf21204881f39bee007c6b81974abde
Jay Solanki <j194solanki@gmail.com>
2022-09-28 16:29:14 +0530

    availbility check

--- src/Components/Cars/CarDetail.js
+++ src/Components/Cars/CarDetail.js
@@ -5,6 +5,7 @@ import { useNavigate, Link , useParams  } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux"
 import { setAlertShow } from '../../ReduxStore/Action';
 import Loading from '../General/loading';
+import DatePicker from "react-datepicker";
 
 export class CarDetail extends Component {
     constructor() {
@@ -15,7 +16,9 @@ export class CarDetail extends Component {
             EpricePerHour: 0,
             EpricePerKm: 0,
             loading:true,
-            WalletCreated:false
+            WalletCreated:false,
+            start_date_time:new Date(),
+            end_date_time:new Date()
         }
         this.getData = this.getData.bind(this)
         this.EHourPlus = this.EHourPlus.bind(this)
@@ -23,6 +26,7 @@ export class CarDetail extends Component {
         this.EKmPlus = this.EKmPlus.bind(this)
         this.EKmMinus = this.EKmMinus.bind(this)
         this.getEstimatedPrice = this.getEstimatedPrice.bind(this)
+        this.checkAvailiblity=this.checkAvailiblity.bind(this)
     }
     getData(){
         axios.get('/car/id/' + this.props.params.id).then(response => {
@@ -104,6 +108,27 @@ export class CarDetail extends Component {
         }
         cal.innerHTML = text
     }
+    checkAvailiblity(e){
+        e.preventDefault()
+        console.log(this.state.start_date_time)
+        axios.get('/booking/car/byDateRange/'+this.state.car._id+'/'+this.state.start_date_time+'/'+this.state.end_date_time).then(response=>{
+            console.log(response)
+            if(response){
+                let avl = document.getElementById('availableStatus')
+                if(response.data.available){
+                    avl.innerHTML ='<h4 style="color:green"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-patch-check" viewBox="0 0 16 16">'+
+                    '<path fillRule="evenodd" d="M10.354 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>'+
+                    '<path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911l-1.318.016z"/>'+
+                  '</svg>Available</h4>' 
+                }else{
+                    avl.innerHTML ='<h4 style="color:red"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-emoji-frown" viewBox="0 0 16 16">'+
+                    '<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'+
+                   '<path d="M4.285 12.433a.5.5 0 0 0 .683-.183A3.498 3.498 0 0 1 8 10.5c1.295 0 2.426.703 3.032 1.75a.5.5 0 0 0 .866-.5A4.498 4.498 0 0 0 8 9.5a4.5 4.5 0 0 0-3.898 2.25.5.5 0 0 0 .183.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>'+
+                  '</svg>Not Available</h4>' 
+                }
+            }
+        })
+    }
     render() {
         const darkMode = {
             background: this.context.generalContext.mode === 'dark' ? '#061020' : 'white',
@@ -167,6 +192,13 @@ export class CarDetail extends Component {
                                         </div>
                                         <button className="btn btn-success w-100 my-5" disabled={this.state.EpricePerHour <= 0 || this.state.EpricePerKm <= 0} onClick={this.getEstimatedPrice}>Calculate Estimated Price</button>
                                         <h5 id="Calculation"> </h5>
+
+                                        <h3 className='m-4'> Check Availiblity </h3>
+                                        Select Pick Up Date & time: <DatePicker minDate={Date.now()} showTimeSelect selected={this.state.start_date_time} onChange={(date) => { this.setState({ start_date_time: date }); if (date > this.state.end_date_time) { this.setState({ end_date_time: date }) } }} /> <br /> <br />
+                                        Select Drop Up Date & time: <DatePicker minDate={this.state.start_date_time} showTimeSelect selected={this.state.end_date_time} onChange={(date) => this.setState({ end_date_time: date })} /> <br /> <br />
+            
+                                        <button className="btn btn-success w-100 my-5" onClick={this.checkAvailiblity}>Check Status</button>
+                                        <h5 id="availableStatus"> </h5>
                                     </>
                                 }
                             </div>
